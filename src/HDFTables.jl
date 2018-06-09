8222b4165e923011fc7ad9d3f518cf5efca843fe
Damien Drix <damien.drix@gmail.com>
2018-06-09 12:19:28 +0100

    free h5 type after use

--- src/HDFTables.jl
+++ src/HDFTables.jl
@@ -12,6 +12,7 @@ export Table
 
 
 function table_type(obj::HDF5Dataset)
+    """ Builds a Tuple type for a compound dataset """
     objtype = HDF5.datatype(obj)
     class_id = HDF5.h5t_get_class(objtype.id)
     if class_id == HDF5.H5T_COMPOUND
@@ -28,38 +29,39 @@ function table_type(obj::HDF5Dataset)
         T = Tuple{field_types...}
         return T, tuple(field_names...)
     else
-        error("Dataset is not a table")
+        error("Dataset is not a table: $obj")
     end
 end
 
 
-function make_memtype(T::Type{<:Tuple}, field_names)
-    memtype_id = HDF5.h5t_create(HDF5.H5T_COMPOUND, sizeof(T))
-    for (i, ftype) in enumerate(T.types)
-        HDF5.h5t_insert(memtype_id, string(field_names[i]), fieldoffset(T,i), HDF5.datatype(ftype))
-    end
-    memtype_id
-end
-
-# Read compound type
 function read_table(obj::HDF5Dataset)
+    # Build a Julia Tuple type for the table elements:
     T, field_names = table_type(obj)
+    @assert isbits(T)
 
-    # Build a datatype that reflects the memory layout of the Julia type:
-    memtype_id = make_memtype(T, field_names)
-    
-    # Allocate the destination array:
-    space = HDF5.dataspace(obj);
-    dims, maxdims = HDF5.get_dims(space)
-    data = Array{T}(dims...)
+    # Build a HDF5 datatype that reflects the memory layout of the Julia type:
+    memtype_id = HDF5.h5t_create(HDF5.H5T_COMPOUND, sizeof(T))
+    try
+        for (i, ftype) in enumerate(T.types)
+            HDF5.h5t_insert(memtype_id, string(field_names[i]), fieldoffset(T,i), HDF5.datatype(ftype))
+        end
 
-    # Read the data using HDF5's built-in data layout conversion:
-    status = HDF5.readarray(obj, memtype_id, data)
-    status < 0 && error("Couldn't read table: $status")
+        # Allocate the destination array:
+        space = HDF5.dataspace(obj);
+        dims, maxdims = HDF5.get_dims(space)
+        data = Array{T}(dims...)
 
-    Table(field_names, data)
+        # Read the data using HDF5's built-in data layout conversion:
+        status = HDF5.readarray(obj, memtype_id, data)
+        status < 0 && error("Couldn't read table: $status")
+
+        return Table(field_names, data)
+    finally
+        HDF5.h5t_close(memtype_id)
+    end
 end
 export read_table
+
 
 column(table::Table, i::Int) = collect(row[i] for row in table.rows)
 
